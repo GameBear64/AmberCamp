f9af53986eec29ae30172b8783b91a6fa4cd1822
TeodoraNS <teodoranikiforova14@gmail.com>
2023-06-27 17:49:09 +0300

    AC-2 - created register page and implemented snackbars

--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -11,12 +11,16 @@ export function useFetch({ url, requireAuth = true, method, body }) {
   if (requireAuth) {
     options = {
       headers: {
-        jwt: window.localStorage.getItem('jwt'),
+        jwt: window.localStorage.getItem(`${import.meta.env.VITE_LOCAL_STORAGE_NAME}`),
         'content-type': 'application/json',
       },
+
       ...options,
     };
   }
 
-  return fetch(`${baseURL}:${import.meta.env.VITE_SERVER_PORT}/${url}`, options).then((res) => res.json());
+  return fetch(`${baseURL}:${import.meta.env.VITE_SERVER_PORT}/${url}`, options).then(async (res) => ({
+    status: res.status,
+    message: await res.json(),
+  }));
 }
