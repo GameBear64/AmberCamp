8a8ab2ba31be1981512d6ebd6e16fe72aff903d7
TeodoraNS <catxrin@gmail.com>
2023-08-01 09:09:43 +0300

    improved the design of login and register page

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import Router from './routes/Router/Router';
+import Router from './routes/Router';
 const Main = () => <Router />;
 
 ReactDOM.createRoot(document.getElementById('root')).render(<Main />);
--- src/routes/Register/Register.jsx
+++ src/routes/Register/Register.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useFetch } from './../../utils/useFetch';
 import { errorSnackBar, successSnackBar } from '../../utils/snackbars';
+import { Link } from 'react-router-dom';
+import resizeScreen from '../../utils/resizeScreen';
 
 export default function Register() {
   const navigate = useNavigate();
@@ -9,6 +11,7 @@ export default function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatRassword, setRepeatPassword] = useState('');
+  const screenSize = resizeScreen();
 
   const registerUser = () => {
     useFetch({
@@ -32,45 +35,68 @@ export default function Register() {
   };
 
   return (
-    <div className=" text-center max-w-md grid m-auto  justify-items-center mt-48 ">
-      <h1 className="text-slate-500 text-4xl pb-8">Amber Camp Register</h1>
-      <div className=" grid  text-center grid-cols-2  gap-4">
-        <input
-          placeholder="@handle"
-          className=" border-solid border-4 rounded col-span-2  border-slate-600 p-2 "
-          type="text"
-          onChange={(e) => {
-            setHandle(e.target.value);
-          }}
-        />
-        <input
-          placeholder="email@ac.com"
-          className="  border-solid border-4 col-span-2 rounded border-slate-600 p-2 "
-          type="text"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <input
-          placeholder="password"
-          className="border-solid border-4 rounded  border-slate-600 p-2"
-          type="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <input
-          placeholder="passsword again"
-          className="border-solid border-4  border-slate-600 p-2"
-          type="password"
-          onChange={(e) => {
-            setRepeatPassword(e.target.value);
-          }}
-        />
-        <button onClick={() => registerUser()} className="border-solid border-4 p-2 rounded-3xl  border-slate-600">
-          Register
-        </button>
+    <div className="grid m-auto grid-cols-2 h-screen lg:grid-cols-1">
+      <div className="px-4">
+        <div className=" text-center max-w-md m-auto flex flex-col justify-items-center mt-52">
+          <h1 className=" font-medium text-4xl pb-8">Amber Camp Register</h1>
+          <div className=" flex  flex-col space-y-4  ">
+            <div className="flex flex-col text-left">
+              <label className="font-semibold text-grey-darkest">Handler</label>
+              <input
+                placeholder="@handle"
+                className="bg-gray-200 text-base col-span-2 rounded border-slate-600 p-3 "
+                type="text"
+                onChange={(e) => {
+                  setHandle(e.target.value);
+                }}
+              />
+              <label className="font-semibold text-grey-darkest">Email</label>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="email@ac.com"
+                className="bg-gray-200 text-base col-span-2 rounded border-slate-600 p-3 "
+                type="text"
+              />
+            </div>
+
+            <div className="flex font-semibold flex-col text-left ">
+              <label className="text-grey-darkest ">Password</label>
+              <div className=" flex flex-row justify-between lg:flex-col gap-4">
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  className="bg-gray-200  text-base border-slate-600 p-3"
+                  type="password"
+                />
+                <input
+                  placeholder="Repeat Password"
+                  className="bg-gray-200  text-base border-slate-600 p-3"
+                  type="password"
+                  onChange={(e) => {
+                    setRepeatPassword(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <button
+            onClick={() => registerUser()}
+            className="uppercase border-solid bg-orange-700 border-2 p-2 mt-5 font-semibold text-lg rounded-md text-white ">
+            Register
+          </button>
+          <Link to={'/user/login'} className="text-right mt-2 font-medium text-blue-700">
+            Already have an account?
+          </Link>
+        </div>
       </div>
+      {screenSize >= 1023 && (
+        <div className="flex bg-gray-200 items-center justify-center text-center h-screen ">
+          <div>
+            <img className="object-contain" src="../bam.png" />
+            <h2 className="font-medium text-4xl pb-8">Amber Camp</h2>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
