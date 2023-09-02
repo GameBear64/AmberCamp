599d96e65635d786818ae24c31526075ea9db049
Vladimir <vlpetrov02@gmail.com>
2023-09-02 10:55:09 +0300

    AX-26 fixing small errors

--- src/components/Form/FormInputs/IconInput.jsx
+++ src/components/Form/FormInputs/IconInput.jsx
@@ -1,15 +1,16 @@
-import IconInput from '../Inputs/IconInput';
-import { Field } from 'react-final-form';
+// import IconInput from '../Inputs/IconInput';
+// import { Field } from 'react-final-form';
 
 export default function IconInputField({ label, type = 'text', icon, width = 'w-60', invalid, name, styles }) {
   return (
-    <Field
-      name={name}
-      render={({ input }) => (
-        <div className={styles}>
-          <IconInput type={type} width={width} label={label} icon={icon} input={input} invalid={invalid} />
-        </div>
-      )}
-    />
+    <h1>fix me</h1>
+    // <Field
+    //   name={name}
+    //   render={({ input }) => (
+    //     <div className={styles}>
+    //       <IconInput type={type} width={width} label={label} icon={icon} input={input} invalid={invalid} />
+    //     </div>
+    //   )}
+    // />
   );
 }
